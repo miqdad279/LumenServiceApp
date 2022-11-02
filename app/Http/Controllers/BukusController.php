7dda28fb0c1789178e33e678189bc997e89c6731
miqdad279 <miqdadabdillah279@gmail.com>
2022-11-02 21:20:38 +0700

    Tugas 4

--- app/Http/Controllers/BukusController.php
+++ app/Http/Controllers/BukusController.php
@@ -63,34 +63,4 @@ class BukusController extends Controller {
         
         return response()->json($buku, 200);
     }
-
-    
-    // public function update(Request $request, $id)
-    // {
-        //     $acceptHeader = $request->header('Accept');
-        
-        //     if($acceptHeader === 'application/json' || $acceptHeader === 'application/xml'){
-            //         $contentTypeHeader = $request->header('Content-Type');
-            
-            //         if($contentTypeHeader === 'application/json'){
-                //             $input = $request->all();
-                
-    //             $buku = Buku::find($id);
-
-    //             if(!$buku){
-    //                 abort(404);
-    //             }
-
-    //             $buku->fill($input);
-    //             $buku->save();
-
-    //             return response()->json($buku, 200);
-    //         } else {
-    //             return response('Unsupported Media Type', 415);
-    //         }
-    //     } else {
-    //         return response('Not Acceptable', 486);
-    //     }
-    // }
-
 }
