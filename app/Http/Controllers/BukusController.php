7a0fa6493fb8f88d7d2464952fce8e33f3690540
miqdad279 <miqdadabdillah279@gmail.com>
2022-11-04 00:05:58 +0700

    Tugas 5

--- app/Http/Controllers/BukusController.php
+++ app/Http/Controllers/BukusController.php
@@ -7,23 +7,59 @@ use Illuminate\Http\Request;
 
 class BukusController extends Controller {
 
-    public function index(){
+    public function index(Request $request){
 
-        $bukus = Buku::OrderBy("id", "DESC")->paginate(10);
+        $acceptHeader = $request->header('Accept');
 
-        $outPut = [
-            "message" => "bukus",
-            "result" => $bukus
-        ];
+        if($acceptHeader === 'application/json' || $acceptHeader === 'application/xml'){
 
-        return response()->json($bukus, 200);
+            $bukus = Buku::OrderBy("id", "DESC")->paginate(10);
+
+            if($acceptHeader === 'application/json') {
+
+                return response()->json($bukus, 200);
+            } else {
+
+                $xml = new \SimpleXMLElement('<bukus/>');
+                foreach ($bukus->items('data') as $item) {
+                    $xmlItem = $xml->addChild('buku');
+
+                    $xmlItem->addChild('id', $item->id);
+                    $xmlItem->addChild('buku_id', $item->buku_id);
+                    $xmlItem->addChild('judul_buku', $item->judul_buku);
+                    $xmlItem->addChild('penulis', $item->penulis);
+                    $xmlItem->addChild('deskripsi', $item->deskripsi);
+                    $xmlItem->addChild('harga', $item->harga);
+                    $xmlItem->addChild('rilis', $item->rilis);
+                    $xmlItem->addChild('created_at', $item->created_at);
+                    $xmlItem->addChild('updated_at', $item->updated_at);
+                }
+                return $xml->asXML();
+            }
+        } else{
+            return response('Not Acceptable!', 406);
+        }
     }
     
     public function store(Request $request){
-        $input = $request->all();
-        $buku = Buku::create($input);
 
-        return response()->json($buku, 200);
+        $acceptHeader = $request->header('Accept');
+
+        if($acceptHeader === 'application/json' || $acceptHeader === 'application/xml'){
+
+            $contentTypeHeader = $request->header('Content-Type');
+
+            if($contentTypeHeader === 'application/json') {
+                $input = $request->all();
+                $buku = Buku::create($input);
+
+                return response()->json($buku, 200);
+            } else {
+                return response('Unsupported Media Type', 415);
+            }
+        } else {
+            return response('Not Acceptable', 406);
+        } 
     }
 
     public function show($id){
