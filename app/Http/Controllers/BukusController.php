1227ecd8c3f837ead7e2b90f31a4ea15785403d6
miqdad279 <miqdadabdillah279@gmail.com>
2022-12-04 09:48:03 +0700

    Tugas 8

--- app/Http/Controllers/BukusController.php
+++ app/Http/Controllers/BukusController.php
@@ -5,13 +5,28 @@ namespace App\Http\Controllers;
 use App\Models\Buku;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Validator;
 
 
 class BukusController extends Controller {
 
     public function index(Request $request){
-        $bukus = Buku::where(['id' => Auth::user()->id])->OrderBy("id", "DESC")->paginate(2)->toArray();
+
+        if(Gate::denies('read-buku')) {
+            return response()->json([
+                'success' => false,
+                'status' => 403,
+                'message' => 'You are unauthorized'
+            ], 403);
+        }
+
+        if(Auth::user()->role === 'admin') {
+            $bukus = Buku::OrderBy("id", "DESC")->paginate(2)->toArray();
+        }else {
+            $bukus = Buku::where(['id' => Auth::user()->id])->OrderBy("id", "DESC")->paginate(2)->toArray();
+        }
+
         $response = [
             "total_count" => $bukus["total"],
             "limit" => $bukus["per_page"],
@@ -26,6 +41,19 @@ class BukusController extends Controller {
     
     public function store(Request $request){
         $input = $request->all();
+        $buku = Buku::create($input);
+
+        // Authtorization
+
+        if(Gate::denies('create-buku', $buku)) {
+            return response()->json([
+                'success' => false,
+                'status' => 403,
+                'message' => 'You are unauthorized',
+            ]);
+        }
+        // authorization end
+
         $validationRules = [
             // 'buku_id' => 'required|exists:users,id',
             'buku_id' => 'required|min:3',
@@ -53,6 +81,17 @@ class BukusController extends Controller {
             abort(404);
         }
 
+        // Authtorization
+
+        if(Gate::denies('read-detail-buku', $buku)) {
+            return response()->json([
+                'success' => false,
+                'status' => 403,
+                'message' => 'You are unauthorized',
+            ]);
+        }
+        // authorization end
+
         return response()->json($buku, 200);
     }
 
@@ -63,6 +102,18 @@ class BukusController extends Controller {
         if(!$buku){
             abort(404);
         }
+
+        // Authtorization
+        // check if current user is authorized to do this action
+
+        if(Gate::denies('update-buku', $buku)) {
+            return response()->json([
+                'success' => false,
+                'status' => 403,
+                'message' => 'You are unauthorized',
+            ]);
+        }
+        // authorization end
 
         $validationRules = [
             'buku_id' => 'required|min:3',
@@ -92,10 +143,21 @@ class BukusController extends Controller {
         if(!$buku){
             abort(404);
         }
+
+        // Authtorization
+
+        if(Gate::denies('delete-buku', $buku)) {
+            return response()->json([
+                'success' => false,
+                'status' => 403,
+                'message' => 'You are unauthorized',
+            ]);
+        }
+        // authorization end
         
         $buku->delete();
-        $message = ['message' => 'deleted successfully', 'buku_id'];
+        $message = ['message' => 'deleted successfully', 'id' => $id];
         
-        return response()->json($buku, 200);
+        return response()->json($message, 200);
     }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\User;
+use App\Models\Buku;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -29,6 +30,57 @@ class AuthServiceProvider extends ServiceProvider
         // application. The callback which receives the incoming request instance
         // should return either a User instance or null. You're free to obtain
         // the User instance via an API token or any other method necessary.
+
+        /**
+         * Register policy
+         */
+
+         // Register Buku policy
+         Gate::define('read-buku', function ($user) {
+            return $user->role == 'editor' || $user->role == 'admin';
+         });
+
+         Gate::define('update-buku', function ($user, $buku) {
+            if($user->role == 'admin'){
+                return true;
+            }elseif($user->role == 'editor'){
+                return $buku->id == $user->id;
+            }else{
+                return false;
+            }
+         });
+
+         Gate::define('read-detail-buku', function($user, $buku) {
+            if($user->role = 'admin'){
+               return true; 
+           } else if ($user->role = 'editor'){
+               return $buku->id == $user->id; 
+           } else {
+               return false; 
+           }
+       });
+
+       Gate::define('create-buku', function($user, $buku) {
+            if($user->role = 'admin'){
+               return true; 
+           } else if ($user->role = 'editor'){
+               return $buku->id == $user->id; 
+           } else {
+               return false; 
+           }
+       });
+
+       Gate::define('delete-buku', function($user, $buku) {
+           if($user->role = 'admin'){
+              return true; 
+          } else if ($user->role = 'editor'){
+              return $buku->id == $user->id; 
+          } else {
+              return false; 
+          }
+      });
+
+         /** Register policy End */
 
         $this->app['auth']->viaRequest('api', function ($request) {
             if ($request->input('api_token')) {
